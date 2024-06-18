424cda3020cb8e6164c7f2c9f54ad61dc1aecc25
KhanzaidGithub <zaidscestudent@gmail.com>
2024-06-18 00:46:57 -0700

    JS Function

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// });
+
+
+let elemcon = document.getElementById('elem-container');
+let fixed = document.querySelector('#fixed-img');
+
+elemcon.addEventListener('mouseenter', () => {
+    fixed.style.display = 'block'
 });
+
+elemcon.addEventListener('mouseleave', () => {
+    fixed.style.display = 'none'
+});
+
+// let elems = document.getElementById('elem1');
+// elems.addEventListener('mouseenter', () => {
+//     let image = elems.getAttribute('data-image');
+//     fixed.style.backgroundImage = `url(${image})`
+// })
+
+let elems = document.querySelectorAll('.elem');
+elems.forEach((e) => {
+    e.addEventListener('mouseenter', () => {
+        let image = e.getAttribute('data-image');
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
